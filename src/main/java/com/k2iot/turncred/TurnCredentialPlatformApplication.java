02423c4b001c58e2ee43f0b37aad8966f45e8d53
KuuKuu <quoc20121998@gmail.com>
2026-08-21 10:06:50 +0700

    chore: scaffold Spring Boot MVC app with virtual threads and local infra

---
+++ src/main/java/com/k2iot/turncred/TurnCredentialPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.k2iot.turncred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnCredentialPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurnCredentialPlatformApplication.class, args);
+    }
+}
